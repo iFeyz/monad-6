f02ebce6f5747201f19db39f205b4b5febf37ca1
iFeyz <latortuesauvage@gmail.com>
2025-07-19 11:32:25 +0200

    fixing lenght

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Run project
 npm i 
 npm run dev 
 
+//
+Adding 2 camera a orbital one and a player one
+Adding multiple object 
+Adding rapier physic ans sync player
+Adding skyboxe and light
--- src/Components/ConnectedUser.tsx
+++ src/Components/ConnectedUser.tsx
@@ -8,7 +8,7 @@ export default function ConnectedUser() {
             <h1 className="text-sm font-bold ">Connected User</h1>
             <ul className="text-sm gap-2 flex flex-1 items-start max-w-[300px] w-full">
                 {connectedUsers.map((user) => (
-                    <li className="bg-white/30 max-w-[300px] w-full backdrop-blur-xl p-2 rounded-md" key={user.userId}>{user.nickname.length > 10 ? user.nickname.slice(0, 10)+"..." : user.nickname}</li>
+                    <li className="bg-white/30 max-w-[300px] w-full backdrop-blur-xl p-2 rounded-md" key={user.userId}>{user.nickname && user.nickname.length > 10 ? user.nickname.slice(0, 10)+"..." : user.nickname}</li>
                 ))}
             </ul>
         </div>
